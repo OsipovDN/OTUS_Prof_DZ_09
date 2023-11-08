d47b6d4d8b9f538d5bbeab2d2f63c5532085aee1
OsipovDN <disorder_2@list.ru>
2023-11-08 20:50:36 +0300

    Для начала работ над ассинхронным выводом

--- headers/ISubject.h
+++ headers/ISubject.h
@@ -1,9 +1,12 @@
 #pragma once
+#include <memory>
+#include <string>
+#include <vector>
 
 class ISubject {
 public:
-	virtual void attach(std::shared_ptr<IObserver> ) = 0;
-	virtual void detach(std::shared_ptr<IObserver> ) = 0;
-	virtual void notify(std::vector<std::string>&) = 0;
+	virtual void attach(std::shared_ptr<IObserver> obj) = 0;
+	virtual void detach(std::shared_ptr<IObserver> obj) = 0;
+	virtual void notify(std::vector<std::string>& block) = 0;
 	virtual ~ISubject() {};
 };
