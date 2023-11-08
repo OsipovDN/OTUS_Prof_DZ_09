e4225b64496de90252ec07f303ba46143df1b1bd
OsipovDN <didison88@gmail.com>
2023-11-08 15:17:17 +0300

    Рабоччий шаблон

--- headers/OutToStream.h
+++ headers/OutToStream.h
@@ -4,29 +4,20 @@
 #include "IObserver.h"
 
 
-class ToFile :public IObserver, std::enable_shared_from_this<IObserver>{
-private:
-	std::shared_ptr<ComandDistr> _subject;
-
+class ToFile :public IObserver, std::enable_shared_from_this<ToFile> {
 public:
-	ToFile(ComandDistr &obj){
-		_subject = std::make_shared<ComandDistr>(obj);
+	ToFile(ISubject &obj) {
+		obj.attach(shared_from_this());
 	};
-
-	void update()override {
-		
-	}
-	~ToFile() {
-	};
+	void update()override {};
+	~ToFile() {};
 
 };
 
-class ToCOut :public IObserver, std::enable_shared_from_this<IObserver> {
-private:
-	std::shared_ptr<ComandDistr> _subject;
+class ToCOut :public IObserver, std::enable_shared_from_this<ToCOut> {
 public:
-	ToCOut(ComandDistr &obj) {
-		_subject = std::make_shared<ComandDistr>(obj);
+	ToCOut(ISubject &obj) {
+		obj.attach(shared_from_this());
 	};
 	void update()override {};
 	~ToCOut() {};
