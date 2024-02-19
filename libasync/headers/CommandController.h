df81e5fcef4057f2ba5ffb804992c8b1e948ed1b
OsipovDN <didison88@gmail.com>
2024-02-19 12:21:52 +0300

    Исправление codeStyle

--- libasync/headers/CommandController.h
+++ libasync/headers/CommandController.h
@@ -15,25 +15,25 @@ class PackageSender;
 
 namespace Controller
 {
-	class ComandController {
+	class CommandController {
 	private:
 		std::unique_ptr<Sender::PackageSender> _msgQueue;
 
-		PullBlock st_pl_cmd;
-		PullBlock dn_pl_cmd;
-		size_t scope_block;
-		bool is_open;
+		PullBlock _statPull;
+		PullBlock _dynamPull;
+		size_t _scopeBlockCount;
+		bool _isOpen;
 		std::list<std::unique_ptr<IObserver>> _observers;
 
 		bool isScope(const std::string& str);
-		void addStBlock(const std::string& str);
+		void addStatBlock(const std::string& str);
 		void addDynBlock(PullBlock& obj);
 
 	public:
 
-		ComandController(std::unique_ptr<Sender::PackageSender> q,std::size_t count);
-		~ComandController() {};
-		void addComand(std::string cmd);
+		CommandController(std::unique_ptr<Sender::PackageSender> q,std::size_t count);
+		~CommandController() {};
+		void addCommand(std::string cmd);
 
 	};
 }
